442cec8e02d74bdfa609d343626f9b5625d467f7
Bonefo <joacod.10@gmail.com>
2024-03-14 16:41:45 -0300

    botonesvista1

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-$(document).ready(function() {
-    $('.men-digital, .qr-codes-personalizados').click(function() {
-        // Elimina la clase "seleccionado" de todos los botones
-        $('.men-digital, .qr-codes-personalizados').removeClass('seleccionado');
-        
-        // Agrega la clase "seleccionado" solo al botón que se hizo clic
-        $(this).addClass('seleccionado');
+document.addEventListener('DOMContentLoaded', function() {
+    var textoCambiarFondo = document.getElementById('boton-1');
+    var textoCambiarFondo2 = document.getElementById('boton-2');
+    var textoCambiarFondo3 = document.getElementById('boton-3');
+    var textoCambiarFondo4 = document.getElementById('boton-4');
+    var claseConFondo = document.querySelector('.uncle-thrice-removed-nodes');
+
+    textoCambiarFondo.addEventListener('click', function() {
+        // Cambiar el fondo de la clase
+        claseConFondo.style.backgroundImage = 'url("./public/qrcodespersonalizadoslandingpage.png")';
     });
+
+    textoCambiarFondo2.addEventListener('click', function() {
+        // Cambiar el fondo de la clase
+        claseConFondo.style.backgroundImage = 'url("./public/ProfileMenuReservaciones.png")';
+    });
+
+    textoCambiarFondo3.addEventListener('click', function() {
+        // Cambiar el fondo de la clase
+        claseConFondo.style.backgroundImage = 'url("./public/landingpanaderia-1@2x.png")';
+    });
+
+    textoCambiarFondo4.addEventListener('click', function() {
+        // Cambiar el fondo de la clase
+        claseConFondo.style.backgroundImage = 'url("./public/calendario.png")';
+    });
+
 });
